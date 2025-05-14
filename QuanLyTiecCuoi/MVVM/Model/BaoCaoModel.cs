f7ee32d5be67ff516128bc999f0b39e3aafbf9e3
phongnq424 <ngcthanh1567@gmail.com>
2025-05-15 01:39:04 +0700

    Commit cho an toàn

---
+++ QuanLyTiecCuoi/MVVM/Model/BaoCaoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTiecCuoi.MVVM.Model
+{
+    internal class BaoCaoModel
+    {
+        public int Thang { get; set; }
+
+        public int Nam { get; set; }
+        public double TongDoanhThu { get; set; }
+        public int TongTiecCuoi { get; set; }
+        public List<ChiTietBaoCaoModel> ChiTietBaoCao { get; set; }
+
+        public BaoCaoModel()
+        {
+            ChiTietBaoCao = new List<ChiTietBaoCaoModel>();
+        }
+
+    }
+}
--- QuanLyTiecCuoi/MVVM/View/HoaDon/ChiTietHoaDonWindow.xaml.cs
+++ QuanLyTiecCuoi/MVVM/View/HoaDon/ChiTietHoaDonWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace QuanLyTiecCuoi.MVVM.View.HoaDon
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
