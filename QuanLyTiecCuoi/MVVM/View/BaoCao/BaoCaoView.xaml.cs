fedf65d09be51165fd596e975ac8b0ac5bc80d21
phongnq424 <ngcthanh1567@gmail.com>
2025-06-13 01:15:06 +0700

    Sửa code

--- QuanLyTiecCuoi/MVVM/View/BaoCao/BaoCaoView.xaml.cs
+++ QuanLyTiecCuoi/MVVM/View/BaoCao/BaoCaoView.xaml.cs
@@ -1,16 +1,8 @@
-<<<<<<< HEAD:QuanLyTiecCuoi/MainWindow.xaml.cs
-﻿using Microsoft.Extensions.DependencyInjection;
-using QuanLyTiecCuoi.MVVM.View;
-using QuanLyTiecCuoi.MVVM.ViewModel;
-using QuanLyTiecCuoi.Services;
-using System.Text;
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
->>>>>>> main:QuanLyTiecCuoi/MVVM/View/BaoCao/BaoCaoView.xaml.cs
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -32,15 +24,7 @@ namespace QuanLyTiecCuoi.MVVM.View.BaoCao
         public BaoCaoPage(BaoCaoViewModel vm)
         {
             InitializeComponent();
-<<<<<<< HEAD:QuanLyTiecCuoi/MainWindow.xaml.cs
-
-            var sanhViewModel = App.AppHost.Services.GetRequiredService<SanhViewModel>();
-            var sanhView = new SanhView(sanhViewModel);
-            MainFrame.Navigate(sanhView);
-
-=======
             DataContext = vm;
->>>>>>> main:QuanLyTiecCuoi/MVVM/View/BaoCao/BaoCaoView.xaml.cs
         }
     }
 }
