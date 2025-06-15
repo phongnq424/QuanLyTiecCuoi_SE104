322e1a36612d0987bc22cbdbaf7ae51a6477022e
phongnq424 <ngcthanh1567@gmail.com>
2025-06-15 16:58:09 +0700

    Thêm lại sl bàn do nãy quên save

---
+++ QuanLyTiecCuoi/Migrations/20250615095742_Them_SLB.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyTiecCuoi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Them_SLB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SoLuongBan",
+                table: "HoaDons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoLuongBan",
+                table: "HoaDons");
+        }
+    }
+}
