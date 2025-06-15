056066a37734b02ed52f17a784f44a5c0e1bf0d3
phongnq424 <ngcthanh1567@gmail.com>
2025-06-15 16:56:05 +0700

    thêm đặt cọc và sl bàn cho hd

---
+++ QuanLyTiecCuoi/Migrations/20250615095520_Thêm đặt cọc và sl bàn cho hd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyTiecCuoi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Thêmđặtcọcvàslbànchohd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PhanTramDatCoc",
+                table: "ThamSos",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhanTramDatCoc",
+                table: "ThamSos");
+        }
+    }
+}
